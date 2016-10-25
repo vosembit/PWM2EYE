711cc856384ffcafa0c7acddac78b7d30d76dcd3
daladno13 <slickindub@gmail.com>
2016-10-26 01:50:47 +0300

    Update LedLight.h

--- LedLight.h
+++ LedLight.h
@@ -2,7 +2,7 @@
   LedLight.h - Micro library for Led driving
   via controllers working under Arduino enviroment.
   Based on built-in function analogWrite(),
-  focused on optical correction of light
+  focused on optical correction of led light
   using gamma maps and non-linear functions.
 
   "- двумя руками черпай, говорю!(с)"
