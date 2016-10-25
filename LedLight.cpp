34f0b90f6ccdae8c9d2fd2086d51522192b93bb7
daladno13 <slickindub@gmail.com>
2016-10-26 01:46:17 +0300

    Add files via upload

---
+++ LedLight.cpp
@@ -0,0 +1,40 @@
+/*
+  LedLight.h - Micro library for Led driving
+  via controllers working under Arduino enviroment.
+  Based on built-in function analogWrite(),
+  focused on optical correction of light
+  using gamma maps and non-linear functions.
+
+  "- двумя руками черпай, говорю!(с)"
+*/
+
+#include <Arduino.h>
+#include "LedLight.h"
+
+LedLight::LedLight(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+
+void LedLight::show(bool sw, uint16_t br) {
+  int n, n_br;
+  int d = 1;
+  if  (sw) n_br = br;
+  else  n_br = 0;
+  if (n_br != c_br) {
+    if (n_br < c_br) {
+      n = c_br - n_br;
+      d = -d;
+    } else {
+      n = n_br - c_br;
+    }
+    for (int i = 0; i < n; i++) {
+      c_br += d;
+      analogWrite(_pin, c_br);
+      delay(1);
+    }
+  }
+  c_br = n_br;
+  analogWrite(_pin, c_br);
+}
---
+++ LedLight.h
@@ -0,0 +1,26 @@
+/*
+  LedLight.h - Micro library for Led driving
+  via controllers working under Arduino enviroment.
+  Based on built-in function analogWrite(),
+  focused on optical correction of light
+  using gamma maps and non-linear functions.
+
+  "- двумя руками черпай, говорю!(с)"
+*/
+
+#ifndef LEDLIGHT_H_
+#define LEDLIGHT_H_
+
+#include <Arduino.h>
+
+class LedLight {
+  public:
+    LedLight(int pin);
+    void show(bool sw, uint16_t br);
+
+  private:
+    uint8_t   _pin;
+    uint16_t  c_br;
+};
+
+#endif
